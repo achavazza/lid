bef55c781bc0d44ae99bb1e1c8994c8d86a614c4
Alejandro Chavazza <alejandrochavazza@gmail.com>
2024-12-17 13:50:01 +0100

    fix

--- src/views/AllQuestions.vue
+++ src/views/AllQuestions.vue
@@ -8,7 +8,7 @@
                     <li v-for="(question, index) in allQuestions" :key="index" class="mb-5">
                         <div class="level mb-0">
                             <div class="level-item level-left">
-                                <h4 class="is-size-5 has-text-weight-semibold">{{ question.question }}</h4>
+                                <h4 class="is-size-5 has-text-weight-semibold breakline">{{ question.question }}</h4>
                             </div>
                             <div class="level-item level-right">
                                 <h4 class="is-size-5 has-text-grey-lighter has-text-weight-semibold">{{ question.id }}</h4>
@@ -31,7 +31,7 @@
                             <li v-for="(question, index) in questions" :key="index" class="mb-5">
                                 <div class="level mb-0">
                                     <div class="level-item level-left" style="max-width:60%">
-                                        <h4 class="is-size-5 has-text-weight-semibold">{{ question.question }}</h4>
+                                        <h4 class="is-size-5 has-text-weight-semibold breakline">{{ question.question }}</h4>
                                     </div>
                                     <div class="level-item level-right">
                                         <h4 class="is-size-5 has-text-grey-lighter has-text-weight-semibold">{{ stateName + ' ' + question.id }}</h4>
